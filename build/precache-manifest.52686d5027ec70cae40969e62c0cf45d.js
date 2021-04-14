e04b4ee5791125fcb41312790f2fcb18ac3ebbb9
Vishal Sharma <vishal.sharma181418@gmail.com>
2021-04-14 16:07:25 +0530

    second

---
+++ build/precache-manifest.52686d5027ec70cae40969e62c0cf45d.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0d05df4f581f5e6329d464309108dc0a",
+    "url": "/Emojipedia/index.html"
+  },
+  {
+    "revision": "d478e7f699f8a4fe0e4f",
+    "url": "/Emojipedia/static/js/2.fc726699.chunk.js"
+  },
+  {
+    "revision": "ab22029ea2e8d343e934",
+    "url": "/Emojipedia/static/js/main.3f63adef.chunk.js"
+  },
+  {
+    "revision": "d1efb9ed1b4494e2553c",
+    "url": "/Emojipedia/static/js/runtime-main.ecbb4640.js"
+  }
+]);
---
+++ src/emojipedia.js
@@ -0,0 +1,53 @@
+const emojipedia = [
+  {
+    id: 1,
+    emoji: "💪",
+    name: "Tense Biceps",
+    meaning:
+      "“You can do that!” or “I feel strong!” Arm with tense biceps. Also used in connection with doing sports, e.g. at the gym."
+  },
+  {
+    id: 2,
+    emoji: "🙏",
+    name: "Person With Folded Hands",
+    meaning:
+      "Two hands pressed together. Is currently very introverted, saying a prayer, or hoping for enlightenment. Is also used as a “high five” or to say thank you."
+  },
+  {
+    id: 3,
+    emoji: "🤣",
+    name: "Rolling On The Floor, Laughing",
+    meaning:
+      "This is funny! A smiley face, rolling on the floor, laughing. The face is laughing boundlessly. The emoji version of “rofl“. Stands for „rolling on the floor, laughing“."
+  },
+  {
+    id: 4,
+    emoji: "✔️",
+    name: "Check Mark",
+    meaning:
+      "A thick check mark / tick displayed on some platforms in a handwritten style. Generally displayed in black, except on Windows where it is displayed in green. Commonly used in lists to signify completed tasks or verification of a statement of fact."
+  },
+  {
+    id: 5,
+    emoji: "🏷️",
+    name: "Label",
+    meaning:
+      "A light-brown or yellow rectangular tag, as labelled and attached to a key. Depicted at various angles punched with a hole, generally with a metal ring or loop of string. Commonly used to represent a price tag or metadata tag as well as for various senses of label."
+  },
+  {
+    id: 6,
+    emoji: "😋",
+    name: "Face Savoring Food",
+    meaning:
+      "A yellow face with smiling eyes and a broad, closed smile with its tongue sticking out of one corner, as if licking its lips in appetite or contentment. Widely used to convey that a food item is delicious. May also express that a person is attractive."
+  },
+  {
+    id: 7,
+    emoji: "🤪",
+    name: "Zany Face",
+    meaning:
+      "A smiley making a silly face. A yellow face with a big grin and wide, white eyes, one larger than the other and in a wild, cockeyed expression. Many platforms, including Apple, depict its tongue stuck out and head tilted; others feature a full-toothed grin, giving it a more unhinged appearance."
+  }
+];
+
+export default emojipedia;
